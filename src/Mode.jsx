3b7dc9f17fd045c55462e42ff41dd6e29b41030a
GiaHuy <giahuytran4205@gmail.com>
2025-06-10 21:06:08 +0700

    define enum Mode

---
+++ src/Mode.jsx
@@ -0,0 +1,16 @@
+
+/**
+ * @readonly
+ * @enum {number}
+ */
+const Mode = Object.freeze({
+    VIEW: 0,
+    SET_ROBOT: 1,
+    DRAW_WALL: 2,
+    DELETE_WALL: 3,
+    SET_DIRT: 4,
+    SET_WEIGHT: 5,
+    SOLVING: 6,
+})
+
+export default Mode
