654e2eea7912cc78e64411b29ce3b45b76bbba41
GiaHuy <giahuytran4205@gmail.com>
2025-06-16 21:18:12 +0700

    Add instruction to see example

--- src/App.jsx
+++ src/App.jsx
@@ -79,6 +79,9 @@ export default function App() {
 		URL.revokeObjectURL(url);
 	}
 
+	// Example for solving mode
+	// First, generate maze with size 10 x 10 (to match with example result)
+	// Then, click "Solve" button to invoke this function
 	async function handleClickSolve(data) {
 		setMode(Mode.SOLVING)
 		// data represent robot_mode, algorithm and heuristic
