9051376482f04c31660a95b75330a64c3f368903
GiaHuy <giahuytran4205@gmail.com>
2025-06-23 16:57:49 +0700

    fix bug

--- src/js/components/SimulationLayer.jsx
+++ src/js/components/SimulationLayer.jsx
@@ -17,6 +17,8 @@ export default function SimulationLayer({ step, ctx, canvas, initRobot = 0, setR
     const isSolving = useRef(false)
     const dirts = useRef(initDirts)
 
+    useEffect(() => { dirts.current = new Set(initDirts) }, [initDirts])
+
     useEffect(() => {
         setRobot(initRobot)
         setDirts(initDirts)
@@ -75,9 +77,10 @@ export default function SimulationLayer({ step, ctx, canvas, initRobot = 0, setR
             const p = canvas.indexToCellCoord(step.path[i])
             ctx.fillRect(p.x, p.y, cellSize, cellSize)
             setRobot?.(step.path[i])
-            if (initDirts?.has(step.path[i])) {
+            if (dirts.current?.has(step.path[i])) {
                 dirts.current.delete(step.path[i])
-                setDirts(dirts.current)
+                console.log(dirts.current)
+                setDirts(new Set(dirts.current))
             }
             await sleep(animSpeed)
         }
